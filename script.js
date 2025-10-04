4be68370bf39c749040e983eceaa445de8b2a4aa
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-10-04 22:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let currentAudio = null;
+
+function playSound(name) {
+  // stop previous audio if playing
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+
+  // create new audio
+  currentAudio = new Audio(`sounds/${name}.mp3`);
+  currentAudio.play();
+}
+
+function stopSound() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+}
